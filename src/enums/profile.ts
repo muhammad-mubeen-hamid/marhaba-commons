ecdc4438fbdff9f4bc0cc1d7d3b9e2615b92ef41
Muhammad Mubeen Hamid <muhammadmubeenhamid@gmail.com>
2024-10-06 00:12:19 +0200

    v1.0.6-o

--- src/enums/profile.ts
+++ src/enums/profile.ts
@@ -1,9 +1,10 @@
-import {AppResponseMessage} from "./index";
+import { AppResponseMessage } from './index';
 
 interface ProfileCode {
     ALL_OKAY: AppResponseMessage;
     PROFILE_UPDATED: AppResponseMessage;
     IMAGE_UPLOADED: AppResponseMessage;
+    PROFILE_NOT_FOUND: AppResponseMessage;
     INVALID_PATH_PARAMETERS: AppResponseMessage;
     INVALID_BODY: AppResponseMessage;
     INTERNAL_SERVER_ERROR: AppResponseMessage;
@@ -22,6 +23,10 @@ export const ProfileCodes: ProfileCode = {
         code: 'PC_02',
         content: 'Image uploaded successfully',
     },
+    PROFILE_NOT_FOUND: {
+        code: 'PC_03',
+        content: 'Profile not found',
+    },
     INVALID_PATH_PARAMETERS: {
         code: 'PC_97',
         content: 'Invalid path parameters',
@@ -33,7 +38,7 @@ export const ProfileCodes: ProfileCode = {
     INTERNAL_SERVER_ERROR: {
         code: 'PC_99',
         content: 'Internal server error',
-    }
+    },
 };
 
 export type ProfileCodesT = typeof ProfileCodes[keyof typeof ProfileCodes];
