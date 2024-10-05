54ff2e87ddb3e867ceed8228f59ab2af57692756
Muhammad Mubeen Hamid <muhammadmubeenhamid@gmail.com>
2024-10-05 13:38:00 +0200

    centralised jwt logic

--- src/types/cognito.ts
+++ src/types/cognito.ts
@@ -1,3 +1,50 @@
+import { JwtPayload } from 'jsonwebtoken';
+
+export interface ParsedJWT {
+    sub: string;
+    email_verified: boolean;
+    iss: string;
+    phone_number_verified: boolean;
+    'cognito:username': string;
+    origin_jti: string;
+    aud: string;
+    event_id: string;
+    token_use: 'id';
+    auth_time: number;
+    phone_number: string;
+    exp: number;
+    iat: number;
+    jti: string;
+    email: string;
+}
+
+export interface ParsedJWK {
+    alg: string;
+    e: string;
+    kid: string;
+    kty: 'EC' | 'RSA';
+    n: string;
+    use: string;
+}
+
+export interface ParsedJWKsResponse {
+    keys: ParsedJWK[];
+}
+
+export interface ParsedJWTHeader {
+    alg: string;
+    kid: string;
+
+    [key: string]: any;
+}
+
+export interface CognitoJwtPayload extends JwtPayload {
+    sub: string;
+    email: string;
+    ['cognito:username']: string;
+}
+
+
 export interface UserResponse {
     username: string;
     creationDate: Date;
--- src/utils/cognito.ts
+++ src/utils/cognito.ts
@@ -1,4 +1,8 @@
-import { SecretsManager } from '@aws-sdk/client-secrets-manager'
+import { SecretsManager } from '@aws-sdk/client-secrets-manager';
+import { CognitoJwtPayload, ParsedJWK, ParsedJWKsResponse, ParsedJWTHeader } from '../types';
+import axios from 'axios';
+import jwkToPem, { JWK } from 'jwk-to-pem';
+import { verify } from 'jsonwebtoken';
 
 const secretsManager = new SecretsManager();
 export const getSecretFromSM = async (secretName: string) => {
@@ -10,5 +14,81 @@ export const getSecretFromSM = async (secretName: string) => {
         }
     } catch (err) {
         throw new Error(`Unable to retrieve secret ${secretName}: ${err}`);
+    }
+};
+
+export const getCognitoPublicKeys = async (jwksUrl: string): Promise<ParsedJWK[]> => {
+    try {
+        console.log('Fetching JWKS keys from Cognito');
+        const { data } = await axios.get<ParsedJWKsResponse>(jwksUrl);
+        console.log('Fetched keys:', data.keys);
+        return data.keys;
+    } catch (err) {
+        throw new Error(`Unable to retrieve public keys from Cognito: ${err}`);
+    }
+};
+
+export const verifyToken = (token: string, keys: ParsedJWK[]): CognitoJwtPayload | null => {
+    // Strip 'Bearer' from the token if present
+    if (token.startsWith('Bearer ')) {
+        token = token.slice(7);
+    }
+
+    console.log('Token after removing Bearer:', token);
+    console.log('Token length:', token.length);
+    console.log('Token parts:', token.split('.'));
+
+    // Parse the header (first part of the JWT)
+    const decodedHeader: ParsedJWTHeader = JSON.parse(
+        Buffer.from(token.split('.')[0], 'base64').toString(),
+    );
+    console.log('Decoded header:', decodedHeader);
+
+    // Find the matching key based on the kid in the token header
+    const key = keys.find((k: ParsedJWK) => k.kid === decodedHeader.kid);
+    console.log('Matching key:', key);
+
+    if (!key) {
+        throw new Error(`No matching key found for kid: ${decodedHeader.kid}`);
+    }
+
+    // interface EC {
+    //     kty: "EC";
+    //     crv: string;
+    //     x: string;
+    //     y: string;
+    // }
+    //
+    // interface ECPrivate {
+    //     kty: "EC";
+    //     crv: string;
+    //     d: string;
+    //     x?: string | undefined;
+    //     y?: string | undefined;
+    // }
+    //
+    // interface RSA {
+    //     kty: "RSA";
+    //     e: string;
+    //     n: string;
+    //     d?: string | undefined;
+    //     p?: string | undefined;
+    //     q?: string | undefined;
+    //     dp?: string | undefined;
+    //     dq?: string | undefined;
+    //     qi?: string | undefined;
+    // }
+    //
+    // type JWK = EC | ECPrivate | RSA;
+
+    const pem = jwkToPem(key as JWK);
+    console.log('PEM:', pem);
+
+    try {
+        console.log('Verifying JWT with PEM');
+        return verify(token, pem, { algorithms: ['RS256'] }) as CognitoJwtPayload;
+    } catch (error) {
+        console.error('Token verification failed:', error);
+        return null;
     }
 };
