ef66247596fb75282b8bfb33bef83e7a7433aaa0
Muhammad Mubeen Hamid <muhammadmubeenhamid@gmail.com>
2024-12-01 23:19:17 +0200

    updated Profile type

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,4 +1,5 @@
 export interface Profile {
+    username: string;       // User's username - acts as the primary key
     email: string;          // User's email address
     contactNumber: string;  // User's contact number
     createdAt: string;      // ISO 8601 timestamp of profile creation
