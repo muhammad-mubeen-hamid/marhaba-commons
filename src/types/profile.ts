7af1ced794d060ac08199cc4cd88543f73914ba8
Muhammad Mubeen Hamid <muhammadmubeenhamid@gmail.com>
2024-12-07 22:55:09 +0200

    version 1.0.7-a-b

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -3,7 +3,7 @@ export interface Profile {
     email: string;          // User's email address
     contactNumber: string;  // User's contact number
     createdAt: string;      // ISO 8601 timestamp of profile creation
-    modifiedAt?: string;    // ISO 8601 timestamp of last modification
+    modifiedAt: string;    // ISO 8601 timestamp of last modification
     firstName?: string;     // User's first name
     lastName?: string;      // User's last name
     dateOfBirth?: string;   // User's date of birth (format: YYYY-MM-DD)
@@ -11,7 +11,6 @@ export interface Profile {
     spokenLanguages?: string[]; // Array of languages spoken by the user
 }
 
-export interface UpdateProfileRequest {
-    profile: Profile;
-    modifiedAt: string;     // ISO 8601 timestamp of the last profile update
+export interface GetProfileResponse {
+    profile: Profile
 }
